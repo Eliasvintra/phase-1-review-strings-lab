628ba8068ab5d75c5b546f8105b60d904eb608c8
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-14 09:15:01 +0300

    Update to completed Assignments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser =  'Python genius';// Write your code in this file!
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-const shortGreeting =  `Welcome, ${currentUser[0]}!`;
+const shortGreeting =  `Welcome, ${currentUser.slice(0, 1)}!`;
